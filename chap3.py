689e74074bf72edf07e22355ce3fa88bd4b737db
diikshaaaa-d <laxmisadgir1987@gmail.com>
2026-06-28 00:24:37 +0530

    Lists code one added

---
+++ chap3.py
@@ -0,0 +1,9 @@
+#Lists in Python [] : A built in data type that stores set of values. It can store elements of diffrent types (integer, float, string,etc.)
+
+marks = [99,88,98,91.2,67,98.9]
+
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[3])
+
